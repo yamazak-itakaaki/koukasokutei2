ff4fbd0b59501afb32da42e0b573660d39381052
tatematuhiroto <ksw2170141@stu.o-hara.ac.jp>
2022-09-27 11:25:14 +0900

    u

--- config/config/settings.py
+++ config/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja' #'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE =  'Asia/Tokyo' #'UTC'
 
 USE_I18N = True
 

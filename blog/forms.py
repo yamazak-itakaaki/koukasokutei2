782f0f9168fe2d2a1fb919f4af0b5d8bdf33aa5f
tatematuhiroto <ksw2170141@stu.o-hara.ac.jp>
2022-09-29 10:45:03 +0900

    message

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,12 @@
+from email import message
 from django import forms
+import os
+from django.core.mail import EmailMessage
 
 class InquiryForm(forms.Form):
     name = forms.CharField(label='氏名', max_length=30)
     email = forms.EmailField(label='メール')
-    subject = forms.CharField(label='件名', max_length=30)
+    css = forms.CharField(label='件名', max_length=30)
     text = forms.CharField(label='本文', widget=forms.Textarea)
 
     def __init__(self, *args, **kwargs):
@@ -15,8 +18,28 @@ class InquiryForm(forms.Form):
         self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスをここに入力してください。'
 
-        self.fields['subject'].widget.attrs['class'] = 'form-control'
-        self.fields['subject'].widget.attrs['placeholder'] = '件名をここに入力してください。'
+        self.fields['css'].widget.attrs['class'] = 'form-control'
+        self.fields['css'].widget.attrs['placeholder'] = '件名をここに入力してください。'
 
         self.fields['text'].widget.attrs['class'] = 'form-control'
         self.fields['text'].widget.attrs['placeholder'] = '本文をここに入力してください。'
+
+    def send_email(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        css = self.cleaned_data['css']
+        text = self.cleaned_data['text']
+
+        subject = 'お問い合わせ{}'.format(css)
+        message = '送信者名: {0}\nメールアドレス: {1}\nメッセージ:\n{2}'.format(name, email, text)
+
+        from_email = os.environ.get('FROM_EMAIL')
+        to_list = [
+            os.environ.get('FROM_EMAIL')
+        ]
+        cc_list = [
+            email
+        ]
+
+        message = EmailMessage(subject=subject, body=message, from_email=from_email, to=to_list, cc=cc_list)
+        message.send()
--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,18 @@ from django.urls import reverse_lazy
 from django.views import generic
 from .forms import InquiryForm
 # Create your views here.
+
+logger = logging.getLogger(__name__)
+
 class IndexView(generic.TemplateView):
     template_name = "base.html"
 
 class InquiryView(generic.FormView):
     template_name="inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('blog:inquiry')
+
+    def form_valid(self, form):
+       form.send_email()
+       logger.info('Inquiry sent by {}'.format(form.cleande_data['name']))
+       return super().form_valid(form)
